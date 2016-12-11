766869b46b69e2c5076b748bb5cbee5acdde422b
Hari-Hara-Sudhan <hariharasudhan1991@gmail.com>
2016-12-12 02:56:56 +0530

    Test cases to check the Search

---
+++ src/twittersearch/hashtag/SearchTest.java
@@ -0,0 +1,14 @@
+package twittersearch.hashtag;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchTest {
+
+	@Test
+	public void testSearch() {
+		//test cases to check the Search servlet 
+	}
+
+}
